e6418a4836ec5cdd134a5d7cb2081d597320311b
vivian joseph <viviansassyboy@gmail.com>
2023-03-16 18:14:15 +0530

    new py

--- src/GCD.java
+++ src/GCD.java
@@ -2,9 +2,7 @@
 public class GCD {
     static int gcd(int n,int m){
         int max = 1;
-        for(int i=1;i<Math.max(n,m);i++){
-            if((n%i==0)&&(m%i==0)) max=i;
-        }
+        for(int i=1;i<Math.min(n,m);i++) if((n%i==0)&&(m%i==0)) max=i;
         return max;
     }
     public static void main(String[] args) {
--- src/chckarrayissorted.java
+++ src/chckarrayissorted.java
@@ -1,8 +1,6 @@
 public class chckarrayissorted {
     static boolean isSorted(int[] arr){
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[i-1]) return false;
-        }
+        for(int i=1;i<arr.length;i++) if(arr[i]<arr[i-1]) return false;
         return true;
     }
     public static void main(String[] args) {
