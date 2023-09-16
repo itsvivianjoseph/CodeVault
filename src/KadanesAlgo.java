80e63ad86105ad87c66e5318185084c1994926c2
vivian joseph <viviansassyboy@gmail.com>
2023-09-16 10:07:07 +0530

    initial

--- src/KadanesAlgo.java
+++ src/KadanesAlgo.java
@@ -1,28 +1,41 @@
 public class KadanesAlgo {
-    static int[] maxsubarray(int[] arr) 
+    // static int[] maxsubarray(int[] arr) 
+    // {
+    //     int start=0,end=0,curr_sum=0,max_sum=Integer.MIN_VALUE;
+    //     for(int i=0;i<arr.length-1;i++)
+    //     {
+    //         curr_sum=0;
+    //         for(int j=0;j<arr.length;j++)
+    //         {
+    //             curr_sum+=arr[j];
+    //             if(curr_sum>max_sum) 
+    //             {
+    //                 max_sum=curr_sum;
+    //                 start=i;
+    //                 end=j;
+    //             }
+    //         }
+    //     }
+    //     System.out.println("MAX SUM : "+max_sum);
+    //     return new int[]{start,end};
+    // }
+    
+    static int maxSubArraySum(int a[])
     {
-        int start=0,end=0,curr_sum=0,max_sum=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length-1;i++)
-        {
-            curr_sum=0;
-            for(int j=0;j<arr.length;j++)
-            {
-                curr_sum+=arr[j];
-                if(curr_sum>max_sum) 
-                {
-                    max_sum=curr_sum;
-                    start=i;
-                    end=j;
-                }
-            }
+        int size = a.length;
+        int maxsum = Integer.MIN_VALUE, currsum = 0;
+ 
+        for (int i = 0; i < size; i++) {
+            currsum = currsum + a[i];
+            if (maxsum < currsum) maxsum = currsum;
+            if (currsum < 0) currsum = 0;
         }
-        System.out.println("MAX SUM : "+max_sum);
-        return new int[]{start,end};
+        return maxsum;
     }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 8, -2, 6, -8, 5};
-        int[] res=maxsubarray(arr);
-        System.out.print("MAX SUBARRAY : ");
-        for(int i=res[0];i<=res[1];i++) System.out.print(arr[i]+" ");
+        int res = maxSubArraySum(arr);
+        System.out.print("MAX SUBARRAY : " + res);
     }
 }
