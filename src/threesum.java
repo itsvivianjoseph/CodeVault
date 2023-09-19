1f5164cab60307c9ec8c65a1e5507d7e0c105031
vivian joseph <viviansassyboy@gmail.com>
2023-09-19 22:16:58 +0530

    sept - 19th progress

--- src/threesum.java
+++ src/threesum.java
@@ -40,7 +40,8 @@ public class threesum {
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int lo = i + 1, hi = nums.length - 1, sum = -nums[i];
                 while (lo < hi) {
-                    if (nums[lo] + nums[hi] == sum) {
+                    if (nums[lo] + nums[hi] == sum) 
+                    {
                         result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
                         while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
                         while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
