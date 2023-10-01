877a8887905ecf806ac1ace91c861ed60fec011c
vivian joseph <viviansassyboy@gmail.com>
2023-10-01 19:53:25 +0530

    oct 1st progress

---
+++ leetcode/LongestSubarrayofOnesAfterDeletingOneElement.java
@@ -0,0 +1,23 @@
+public class LongestSubarrayofOnesAfterDeletingOneElement {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int zerosCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zerosCount++;
+            }
+
+            while (zerosCount > 1) {
+                if (nums[left] == 0) {
+                    zerosCount--;
+                }
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength - 1;
+    }
+}
---
+++ leetcode/PowerOfThree.java
@@ -0,0 +1,24 @@
+public class PowerOfThree {
+    public static void main(String[] args) {
+        int n = 8;
+
+        boolean result = isPowerOfThree(n);
+
+        System.out.println(result);
+    }
+    
+    public static boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        
+        return n == 1;
+    }
+    // method 2 
+    // Check if n is greater than 0 and if 1162261467 is divisible by n without remainder.
+    // return n > 0 && 1162261467 % n == 0;
+}
